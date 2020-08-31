bb5a36cf74010fee56d2951bda15e75261a1c89e
Sachin Nediyanchath <sachin.nediyanchath@gmail.com>
2020-08-30 19:50:40 -0700

    Entry file that uses the 3 classes
    This source code is an entry point
    
    Morse.bin is the file that are converted from

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+/*
+Spring 2018 CIS 29 Larkin
+Lab 1 : Morse
+Name: Sachin Nediyanchath
+IDE: VS 2017
+
+This 
+*/
+
+#include "Binary_translator.h"
+
+int main()
+{
+	Binary_translator Jerry;
+
+	return 0 ; 
+} 
